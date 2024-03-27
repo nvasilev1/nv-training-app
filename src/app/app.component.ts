2da4f57f349aed4f6dc88eb30d6fb3722fd54028
unknown <nvasilev@novacore.bg>
2024-03-27 10:39:53 +0200

    my first commint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nv-training-app';
+  title = 'nv-training-task';
+  isMenuOpen:Boolean = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  allWords:string[] = ['купувач', 'наемател','продавач']  ;
+  wordIndex:number = 0;
+  currentWord:string = "";
+  tmp:number = 1;
+
+
+  ngOnInit() {
+  
+    /*this.interval = setInterval(() => {
+      this.rotateWord(); // Rotate the word inside the setInterval
+      console.log('Word rotated:', this.currentWord); // Log the rotated word
+    }, 1000);*/
+  
+
+  }
+
+
+
+  /*this.interval = setInterval(() => {
+      this.rotateWord(); // Rotate the word inside the setInterval
+      console.log('Word rotated:', this.currentWord); // Log the rotated word
+    }, 1000);
+  rotateWord() {
+    this.wordIndex = (this.wordIndex+1)%this.allWords.length;
+    this.currentWord = this.allWords[this.wordIndex];
+    return this.currentWord
+  }*/
+
 }
